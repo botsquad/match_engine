55a47b6679b70cb047d1325c1c0a812de62b3291
Arjan Scherpenisse <arjan@scherpenisse.net>
2018-08-10 12:25:40 +0200

    Make filter_all/2 work on any Enumerable, not just on lists

--- lib/match_engine/scoring.ex
+++ lib/match_engine/scoring.ex
@@ -5,25 +5,18 @@ defmodule MatchEngine.Scoring do
 
   def score_all(docs, query) do
     docs
-    |> Enum.map(& Map.put(&1, "_match", score(query, &1)))
-    |> Enum.sort_by(&(&1["_match"]["score"]), &Kernel.>/2)
+    |> Enum.map(&Map.put(&1, "_match", score(query, &1)))
+    |> Enum.sort_by(& &1["_match"]["score"], &Kernel.>/2)
   end
 
   def filter_all([], _query) do
     []
   end
-  def filter_all([m | _] = docs, query) when is_map(m) do
-    docs
-    |> Enum.map(&(Map.put(&1, "_match", filter(query, &1))))
-    |> Enum.filter(&(&1["_match"]["score"] > 0))
-    |> Enum.map(& Map.delete(&1, "_match"))
-  end
-  def filter_all(docs, query) when is_list(docs) do
-    docs
-    |> Enum.filter(fn(doc) ->
+
+  def filter_all(docs, query) do
+    Enum.filter(docs, fn doc ->
       result = filter(query, doc)
       result["score"] > 0
     end)
   end
-
 end
